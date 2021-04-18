29746c339e0c05a365e5500a27e64e711b782e62
westhecool <52859033+westhecool@users.noreply.github.com>
2021-04-18 06:51:33 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+echo "disable OS firewall"
+iptables-save > ~/firewall.rules
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+echo "disableing SELinux"
+rm /etc/selinux/config
+echo -e "# This file controls the state of SELinux on the system.\n# SELINUX= can take one of these three values:\n# enforcing - SELinux security policy is enforced.\n# permissive - SELinux prints warnings instead of enforcing.\n# disabled - No SELinux policy is loaded.\nSELINUX=disabled\n# SELINUXTYPE= can take one of these two values:\n# targeted - Only targeted network daemons are protected.\n# strict - Full SELinux protection.\nSELINUXTYPE=targeted" > /etc/selinux/config
+echo "disableing the Network Manager service"
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+echo -e "\nNM_CONTROLLED=no" > /etc/sysconfig/network-scripts/ifcfg-io
+echo -e "\nNM_CONTROLLED=no" > /etc/sysconfig/network-scripts/ifcfg-$1
+systemctl enable network.service
+systemctl start network.service
+echo "installing perl"
+yum -y install perl
+echo "installing nano"
+yum install nano -y
+echo "runing: 'yum update'"
+yum update
+echo "runing: 'cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest'"
+cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
